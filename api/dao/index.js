078bb7d127f7245f0f1db22fbaf728b5daa000ee
vvimo <lyj397456796@qq.com>
2018-02-06 13:36:49 +0800

    init

---
+++ api/dao/index.js
@@ -0,0 +1,9 @@
+var articleDao = require('./article/index.js')
+var loginDao = require('./login/index.js')
+var userDao = require('./user/index.js')
+
+module.exports = {
+  articleDao,
+  loginDao,
+  userDao
+}
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express.Router();
+var ApiDao = require('../api/dao');
+
+
+/* login */
+app.post('/login/add', function (req, res, next) { ApiDao.loginDao.add(req, res, next) })
+app.post('/login/update', function (req, res, next) { ApiDao.loginDao.update(req, res, next) })
+app.post('/login/delete', function (req, res, next) { ApiDao.loginDao.delete(req, res, next) })
+app.post('/login/login', function (req, res, next) { ApiDao.loginDao.queryByUserName(req, res, next) })
+
+/* user */
+app.post('/user/add', function (req, res, next) { ApiDao.userDao.add(req, res, next) })
+app.post('/user/update', function (req, res, next) { ApiDao.userDao.update(req, res, next) })
+app.post('/user/delete', function (req, res, next) { ApiDao.userDao.delete(req, res, next) })
+app.post('/user/queryById', function (req, res, next) { ApiDao.userDao.queryById(req, res, next) })
+app.post('/user/queryAll', function (req, res, next) { ApiDao.userDao.queryAll(req, res, next) })
+
+/* article */
+app.post('/article/add', function (req, res, next) { ApiDao.articleDao.add(req, res, next) })
+app.post('/article/update', function (req, res, next) { ApiDao.articleDao.update(req, res, next) })
+app.post('/article/updateRead', function (req, res, next) { ApiDao.articleDao.updateRead(req, res, next) })
+app.post('/article/updateState', function (req, res, next) { ApiDao.articleDao.updateState(req, res, next) })
+app.post('/article/delete', function (req, res, next) { ApiDao.articleDao.delete(req, res, next) })
+app.post('/article/queryById', function (req, res, next) { ApiDao.articleDao.queryById(req, res, next) })
+app.post('/article/queryAll', function (req, res, next) { ApiDao.articleDao.queryAll(req, res, next) })
+
+module.exports = app;
